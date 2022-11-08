b560011dd6be366618564103c1b189fab70fea16
Michael Chop <007micle007@gmail.com>
2022-11-08 22:43:51 +0300

    Added Redux to the project

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,11 +4,30 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props);
 
-        this.statee = {}
+        this.state = {
+            title: ''
+        }
     }
 
     submitHandler = event => {
         event.preventDefault()
+
+        const {title} = this.state
+        const newPost = {
+            title, id: Date.now().toString()
+        }
+
+        this.setState({title: ''})
+    }
+
+    changeInputHandler = event => {
+        event.persist()
+        this.setState(prev => ({
+            ...prev,
+            ...{
+                [event.target.name]: event.target.value
+            }
+        }))
     }
 
     render() {
@@ -16,7 +35,14 @@ export default class PostForm extends React.Component {
             <form onSubmit={this.submitHandler}>
                 <div className="form-group">
                     <label htmlFor="title" className="form-label">Заголовок поста</label>
-                    <input type="text" className="form-control" id="title"/>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        value={this.state.title}
+                        name={"title"}
+                        onChange={this.changeInputHandler}
+                    />
                 </div>
                 <button className="btn btn-success mt-3" type={"submit"}>Создать</button>
             </form>
--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore} from "redux";
+import {rootReducer} from "./redux/rootReducer";
+import {Provider} from "react-redux";
+
+const store = createStore(rootReducer)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+        <App/>
+    </Provider>
 );
 
 reportWebVitals();
