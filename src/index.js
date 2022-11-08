ea3717ee704f04533e586fff869a7616e034653f
Michael Chop <007micle007@gmail.com>
2022-11-08 23:54:45 +0300

    Refactored post requests, using Redux-Saga

--- src/index.js
+++ src/index.js
@@ -6,9 +6,15 @@ import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "./redux/rootReducer";
 import {Provider} from "react-redux";
 import thunk from 'redux-thunk'
+import createSagaMiddleware from 'redux-saga'
 import {forbiddenWordsMiddleware} from "./redux/middleware";
+import {sagaWatcher} from "./redux/sagas";
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk, forbiddenWordsMiddleware)))
+const saga = createSagaMiddleware()
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk, forbiddenWordsMiddleware, saga)))
+
+saga.run(sagaWatcher)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,4 +1,4 @@
-import {CREATE_POST, FETCH_POSTS, HIDE_ALERT, HIDE_LOADER, SHOW_ALERT, SHOW_LOADER} from "./types";
+import {CREATE_POST, FETCH_POSTS, HIDE_ALERT, HIDE_LOADER, REQUEST_POSTS, SHOW_ALERT, SHOW_LOADER} from "./types";
 
 export function createPost(post) {
     return {
@@ -39,16 +39,19 @@ export function hideAlert() {
 }
 
 export function fetchPosts() {
-    return async dispatch => {
-        try {
-            dispatch(showLoader())
-            const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
-            const json = await response.json()
-            dispatch({type: FETCH_POSTS, payload: json})
-            dispatch(hideLoader())
-        } catch(e) {
-            dispatch(showAlert('Что-то пошло не так'))
-            dispatch(hideLoader())
-        }
+    return {
+        type: REQUEST_POSTS
     }
+    // return async dispatch => {
+    //     try {
+    //         dispatch(showLoader())
+    //         const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
+    //         const json = await response.json()
+    //         dispatch({type: FETCH_POSTS, payload: json})
+    //         dispatch(hideLoader())
+    //     } catch(e) {
+    //         dispatch(showAlert('Что-то пошло не так'))
+    //         dispatch(hideLoader())
+    //     }
+    // }
 }
